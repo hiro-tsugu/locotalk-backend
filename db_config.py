5b02efa89b41b93c328b85658dcb541e898ad6a3
hiro-tsugu <hirotsugu.ito@gmail.com>
2025-04-23 11:17:34 +0900

    Add db_config

--- db_config.py
+++ db_config.py
@@ -1,14 +1,15 @@
-import mysql.connector
+import pymysql
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_connection():
-    return mysql.connector.connect(
-        host=os.getenv('DB_HOST', 'localhost'),
-        user=os.getenv('DB_USER', 'root'),
-        password=os.getenv('DB_PASSWORD', ''),
-        database=os.getenv('DB_NAME', 'locotalk_db'),
-        port=int(os.getenv('DB_PORT', 3306))
+    return pymysql.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        db=os.getenv("MYSQL_DATABASE"),
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
     )
