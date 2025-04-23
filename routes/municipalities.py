3f832898593a74c64b1a5c14308c6b4bfe2112f8
hiro-tsugu <hirotsugu.ito@gmail.com>
2025-04-23 12:01:55 +0900

    Fix municipalities.py

--- routes/municipalities.py
+++ routes/municipalities.py
@@ -19,6 +19,8 @@ def get_connection():
 
 @municipality_bp.route('/municipalities', methods=['GET'])
 def get_all_municipalities():
+    conn = None
+    cursor = None
     try:
         conn = get_connection()
         cursor = conn.cursor()
@@ -38,6 +40,8 @@ def get_all_municipalities():
 
 @municipality_bp.route('/municipality/<id>', methods=['GET'])
 def get_municipality_by_id(id):
+    conn = None
+    cursor = None
     try:
         conn = get_connection()
         cursor = conn.cursor()
@@ -48,7 +52,9 @@ def get_municipality_by_id(id):
         else:
             return jsonify({'error': 'Municipality not found'}), 404
     except Exception as e:
+        print(f"❌ [municipality.py] ID検索エラー: {e}")
         return jsonify({'error': str(e)}), 500
     finally:
         if cursor: cursor.close()
         if conn: conn.close()
+
